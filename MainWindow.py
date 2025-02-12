0fc0e6c59b738c465f031a3ccdb3d14c6ace6da5
Michal Fiejtek <fiejtekmichal@gmail.com>
2025-02-12 13:15:44 +0100

    icon added

--- MainWindow.py
+++ MainWindow.py
@@ -1,9 +1,11 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtGui import QIcon
 from PyQt6.QtCore import QSize
 from Ui_Files.Ui_MainWindow import Ui_MainWindow
 from Meals_Database.meals_database_func import initializeDatabase
 from EditMealsDialog import EditMealsDialog
+import os
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -12,6 +14,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.setWindowTitle("MealGambler")
         self.setFixedSize(QSize(300, 150))
+        self.setWindowIcon(QIcon("icon.png"))
 
         self.editMealsButton.clicked.connect(self.openEditMealsDialog)
     
@@ -23,6 +26,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 if __name__ == "__main__":
     initializeDatabase()
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("icon.png"))
 
     mainWindow = MainWindow()
     mainWindow.show()
